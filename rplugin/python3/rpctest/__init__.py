970950c6c9e1802e8b3c9225618d3d303885a6eb
shuntaka9576 <shuntaka9576@gmail.com>
2019-12-27 01:29:50 +0900

    update

--- rplugin/python3/rpctest/__init__.py
+++ rplugin/python3/rpctest/__init__.py
@@ -1,23 +1,13 @@
 import pynvim as Nvim
-from rpctest.rplugin import Rplugin
 
 
 @Nvim.plugin
 class RpcTestHandlers(object):
     def __init__(self, nvim: Nvim) -> None:
-        self._rplugin = Rplugin(nvim)
+        self.nvim = nvim
 
-    @Nvim.function('_rpctest_init', sync=True)
-    def init_channel(self) -> None:
-        self._rplugin.init_channel()
+    @Nvim.function("_rpctest_init", sync=True)
+    def init() -> None:
+        print("hello")
 
-    # @Nvim.autocmd("VimEnter")
-    # def update(self):
-    #     try:
-    #         self.wid = self.api.workspaces()[0]["id"]
-    #         self.projects = self.get_projects([])
-    #     except ConnectionError:
-    #         self.echo("No network, toggl.nvim is disabled.")
-
-    # def echo(self, msg):
-    #     self.nvim.command("echo '{}'".format(msg))
+    # @Nvim.rpc_export()
